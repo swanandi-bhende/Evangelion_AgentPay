71810c4f415e827537ff15d19f3d2e74ec12505a
Swanandi Bhende <swanandibhende@gmail.com>
2025-10-23 13:27:45 +0530

    chat layout 231025_1327

---
+++ agentpay-frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import ChatInterface from '../components/ChatInterface';
+import './globals.css';
+import '@/components/ChatStyles.css';
+
+export default function Home() {
+  return (
+    <main className="main-container">
+      <div className="background-pattern"></div>
+      <ChatInterface />
+    </main>
+  );
+}
